a91a50a36f59dd8ad98acdf706585c2dce0429a5
Vinicius Verdade <vinicius-verdade@hotmail.com>
2023-07-17 16:08:05 -0300

    Commit inicial com o código Angular frontend

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee, HttpClientService } from '../service/httpclient.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  employees: Employee[] = [];
+   
+  constructor(
+    private httpClientService:HttpClientService
+  ) { }
+
+  ngOnInit() {
+    this.httpClientService.getEmployees().subscribe(
+     response =>this.handleSuccessfulResponse(response),
+    );
+  }
+
+handleSuccessfulResponse(response: Employee[])
+{
+    this.employees=response;
+}
+
+}
